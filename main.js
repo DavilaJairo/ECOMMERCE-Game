9b3dab430251b3e234d6a217308537e69410fcfd
davila_jairo <jairodavila66@gmail.com>
2023-05-01 13:10:40 -0300

    agrege metodo de pago

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 
+
+//------------------------carrito---------------------------------------
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach(button => {
@@ -9,18 +11,18 @@ buttons.forEach(button => {
 function addToCart(event) {
   const product = event.target.getAttribute('data-product');
   const price = event.target.getAttribute('data-price');
-  
-  let cart = JSON.parse(localStorage.getItem('cart')) || [];
-  
+
+  let cart = getCart();
+
   const item = {
     product: product,
     price: price
   };
-  
+
   cart.push(item);
-  
-  localStorage.setItem('cart', JSON.stringify(cart));
-  
+
+  saveCart(cart);
+
   swal({
     title: "producto agregado al carrito de compras",
     Text: "podes seguir comprando",
@@ -29,12 +31,29 @@ function addToCart(event) {
   });
 
   // Actualizar la variable cartItems con la nueva información del carrito del almacenamiento local
-  cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+  cartItems = getCart();
 
   showCart(); // llamada a la función para mostrar el carrito actualizado
 }
 
-let cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+function getCart() {
+  return JSON.parse(localStorage.getItem('cart')) || [];
+}
+
+function saveCart(cart) {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function removeItem(index) {
+  let cart = getCart();
+  cart.splice(index, 1);
+  saveCart(cart);
+  cartItems = getCart();
+  showCart();
+}
+
+//---------------------------------------------------------------
+let cartItems = getCart();
 const cartTable = document.querySelector('#cart-table tbody');
 const cartTotal = document.querySelector('#cart-total');
 const buyButton = document.querySelector('#buy-button');
@@ -42,51 +61,124 @@ const resetButton = document.querySelector('#reset-button');
 
 function showCart() {
   cartTable.innerHTML = '';
-  
+
   let total = 0;
-  
-  cartItems.forEach(item => {
+
+  cartItems.forEach((item, index) => {
     const row = document.createElement('tr');
-    
+
     const product = document.createElement('td');
     product.textContent = item.product;
-    
+
     const price = document.createElement('td');
     price.textContent = `$${item.price}`;
-    
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Eliminar';
+    removeButton.addEventListener('click', () => {
+      removeItem(index);
+    });
+
+    const removeCell = document.createElement('td');
+    removeCell.appendChild(removeButton);
+
     row.appendChild(product);
     row.appendChild(price);
+    row.appendChild(removeCell);
     cartTable.appendChild(row);
-    
+
     total += parseFloat(item.price);
   });
-  
+
   cartTotal.textContent = `$${total}`;
 }
 
 showCart();
-
-buyButton.addEventListener('click', () => {
+//---------------------------------------------------------------
+/*buyButton.addEventListener('click', () => {
   swal({
-    title: "¡Gracias por su compra!",
-    text: "Su compra ha sido procesada",
-    icon: "success",
-    button: "Aceptar"
-  }).then(() => {
-    localStorage.removeItem('cart');
+    title: "Confirmación de compra",
+    html: `<p>Por favor, confirme su compra:</p><p>Nombre: <input id="name" type="text"></p><p>Tipo de pago:
+    <select id="payment-method">
+        <option value="credit-card">Tarjeta de crédito</option>
+        <option value="debit-card">Tarjeta de débito</option>
+        <option value="paypal">PayPal</option>
+        <option value="mercado-pago">Mercado Pago</option>
+    </select></p>`,
+    showCancelButton: true,
+    confirmButtonText: "Confirmar",
+    cancelButtonText: "Cancelar"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const name = document.getElementById("name").value;
+      const paymentMethod = document.getElementById("payment-method").value;
+      const paymentResult = simulatePayment(); // Simula el proceso de pago
 
-    showCart();// llamada a la función para mostrar el carrito actualizado
-    location.reload();
+      // Mostrar un mensaje de éxito o fracaso, dependiendo del resultado del pago simulado
+      if (paymentResult === "success") {
+        swal({
+          title: "¡Gracias por su compra!",
+          text: `Su compra ha sido procesada con éxito con ${paymentMethod}.`,
+          icon: "success",
+          button: "Aceptar"
+        }).then(() => {
+          localStorage.removeItem('cart');
+
+          showCart(); // Llamada a la función para mostrar el carrito actualizado
+          location.reload();
+        });
+      } else {
+        swal({
+          title: "Error al procesar su pago",
+          text: "Lo siento, no pudimos procesar su pago. Por favor, inténtelo de nuevo más tarde.",
+          icon: "error",
+          button: "Aceptar"
+        });
+      }
+    }
   });
 });
 
+function simulatePayment() {
+  // Esta función simula el proceso de pago. Puede devolver "success" o "error" aleatoriamente para probar la pantalla de confirmación.
+  return Math.random() < 0.5 ? "success" : "error";
+}*/
 
-resetButton.addEventListener('click', () => {
-  localStorage.removeItem('cart');
+buyButton.addEventListener('click', () => {
+  const name = prompt('Ingrese su nombre:');
+  if (name) {
+    const paymentMethods = ['tarjeta de credito', 'tarjeta de debito', 'mercado pago', 'paypal'];
+    const paymentMethod = prompt('Seleccione el método de pago: ' + paymentMethods.join(', '));
 
-  // Actualizar la variable cartItems con la nueva información del carrito del almacenamiento local
-  cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+    if (paymentMethods.includes(paymentMethod)) {
+      const cardNumber = prompt('Ingrese el número de su tarjeta de pago:');
 
-  showCart(); // llamada a la función para mostrar el carrito actualizado
-  location.reload();
+      swal({
+        title: "¡Gracias por su compra!",
+        text: `Su compra ha sido procesada con éxito utilizando ${paymentMethod}.`,
+        icon: "success",
+        button: "Aceptar"
+      }).then(() => {
+        localStorage.removeItem('cart');
+        showCart();
+        location.reload();
+      });
+    } else {
+      swal({
+        title: "Error",
+        text: "Método de pago inválido",
+        icon: "error",
+        button: "Aceptar"
+      });
+    }
+  } else {
+    swal({
+      title: "Error",
+      text: "Debe ingresar su nombre",
+      icon: "error",
+      button: "Aceptar"
+    });
+  }
 });
+
+
